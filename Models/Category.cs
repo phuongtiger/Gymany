e6aa77294af5291d6772e6dde0631d9dab736c7f
minhthang29 <minhthang32095@gmail.com>
2024-02-21 15:14:35 +0700

    models

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gymany.Models
+{
+    public class Category
+    {
+         [Key]
+        public string CategoryID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Type { get; set; }
+
+        public string Description { get; set; }
+    }
+}

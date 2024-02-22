f12af7767d2f61084715f29eeaf8fcfa0f1175e8
DeWick <Ducsieuda@gmail.com>
2024-02-22 14:26:54 +0700

    change

--- obj/Debug/net5.0/Gymany.RazorTargetAssemblyInfo.cs
+++ obj/Debug/net5.0/Gymany.RazorTargetAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+978c79220d8b90c0f2d8c08a5468b63c1a4f3428")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gymany.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 

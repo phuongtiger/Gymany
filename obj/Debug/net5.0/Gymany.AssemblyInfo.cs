70cbd66074be2371fa76dc20731a2a153afc256e
Hoàng Huy Phương <hhphuong465@gmail.com>
2024-02-25 19:03:10 +0700

    ok

--- obj/Debug/net5.0/Gymany.AssemblyInfo.cs
+++ obj/Debug/net5.0/Gymany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77e9e0789066c930ac8630c1236a549a4a54d90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

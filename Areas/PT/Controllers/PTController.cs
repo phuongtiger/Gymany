83317ef91e6f42ec2eb948127966904bfff40c42
DeWick <Ducsieuda@gmail.com>
2024-02-25 22:14:22 +0700

    Xong Post nhưng vẫn chưa Login được T_T

--- Areas/PT/Controllers/PTController.cs
+++ Areas/PT/Controllers/PTController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Gymany.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Gymany.Controllers
 {
@@ -16,6 +18,8 @@ namespace Gymany.Controllers
         private string api;
         private string api_GetCustomerByID;
         private string api_post;
+        private string api_PT;
+        private string api_GetPostID;
         public PTController()
         {
             client = new HttpClient();
@@ -24,6 +28,8 @@ namespace Gymany.Controllers
             this.api = "https://localhost:5002/api/Member";
             this.api_GetCustomerByID = "https://localhost:5002/api/Customer/id";
             this.api_post = "https://localhost:5002/api/Post";
+            this.api_PT = "https://localhost:5002/api/PT";
+            this.api_GetPostID = "https://localhost:5002/api/Post/id";
         }
         public async Task<IActionResult> Index()
         {
@@ -51,33 +57,64 @@ namespace Gymany.Controllers
             return View(customer);
         }
 
-        public IActionResult CreatePost()
+        public async Task<IActionResult> CreatePost()
         {
-            // TODO: Your code here
+            ViewBag.PTID = await GetPTNameSelected();
             return View();
         }
 
         [HttpPost]
 
-        public async Task<IActionResult> CreatePost(Post obj){          
-            if(ModelState.IsValid)
+        public async Task<IActionResult> CreatePost(Post obj)
+        {
+            if (ModelState.IsValid)
             {
-                
-                string data=JsonSerializer.Serialize(obj);
-                var content= new StringContent(data, System.Text.Encoding.UTF8,"application/json");
-                HttpResponseMessage response =await client.PostAsync(api, content);
-                if (response.StatusCode==System.Net.HttpStatusCode.Created)
-                return RedirectToAction("Index");
+
+                string data = JsonSerializer.Serialize(obj);
+                var content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(api_post, content);
+                if (response.StatusCode == System.Net.HttpStatusCode.Created)
+                    return RedirectToAction("PostManage");
             }
             return View(obj);
-         }
-        
-        public IActionResult DeletePost()
-        {
-            // TODO: Your code here
-            return View();
         }
-        
+
+        public async Task<ActionResult> DeletePost(int? id)
+        {
+            api_GetPostID = $"https://localhost:5002/api/Post/id?id={id}";
+            HttpResponseMessage respone = await client.GetAsync(api_GetPostID);
+            string data = await respone.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            Post post = JsonSerializer.Deserialize<Post>(data, options);
+            return View(post);
+        }
+        [HttpPost]
+        public async Task<ActionResult> DeletePost(int id){
+            api_GetPostID = $"https://localhost:5002/api/Post/id?id={id}";
+            try
+            {
+                // Tạo yêu cầu DELETE
+                HttpResponseMessage response = await client.DeleteAsync(api_GetPostID);
+
+                // Kiểm tra kết quả trả về từ endpoint API
+                if (response.IsSuccessStatusCode)
+                {
+                    // Xử lý kết quả nếu xóa thành công, ví dụ chuyển hướng đến trang danh sách
+                    return RedirectToAction("PostManage");
+                }
+                else
+                {
+                    // Xử lý kết quả nếu xóa không thành công, ví dụ hiển thị thông báo lỗi
+                    return View("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi nếu có
+                return View("Error");
+            }
+        }
+
 
         public IActionResult BackToLogin()
         {
@@ -101,6 +138,20 @@ namespace Gymany.Controllers
         {
             // TODO: Your code here
             return View();
+        }
+
+        public async Task<List<SelectListItem>> GetPTNameSelected()
+        {
+            HttpResponseMessage respone = await client.GetAsync(api_PT);
+            string data = await respone.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            List<PersonalTrainer> list = JsonSerializer.Deserialize<List<PersonalTrainer>>(data, options);
+            List<SelectListItem> yourData = list.Select(c => new SelectListItem
+            {
+                Value = c.PTID.ToString(), // ID của category là giá trị của mục
+                Text = c.Name // Tên của category là nội dung của mục
+            }).ToList();
+            return yourData;
         }
 
     }
--- obj/Debug/net5.0/Gymany.RazorTargetAssemblyInfo.cs
+++ obj/Debug/net5.0/Gymany.RazorTargetAssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c161e46263c54b97c898ff3a4b6a79700a48588f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb4cc6e09b493fdf3028119b927332f8422dc14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gymany.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 

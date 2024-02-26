0ba7653037e5b6fdf61aff7ad5dc2ca7fa1db828
Hoàng Huy Phương <hhphuong465@gmail.com>
2024-02-26 13:20:42 +0700

    Update

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Gymany.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -13,25 +14,45 @@ namespace Gymany.Controllers
         public IActionResult Login()
         {
             // TODO: Your code here
-            return View();
+            List<Notification> notifications = HttpContext.Session.GetObjectFromJson<List<Notification>>("Notifications");
+            var viewModel = new ListModels
+            {
+                Notifications = notifications
+            };
+            return View(viewModel);
         }
 
         public IActionResult PTLogin()
         {
             // TODO: Your code here
-            return View();
+            List<Notification> notifications = HttpContext.Session.GetObjectFromJson<List<Notification>>("Notifications");
+            var viewModel = new ListModels
+            {
+                Notifications = notifications
+            };
+            return View(viewModel);
         }
 
         public IActionResult Register()
         {
             // TODO: Your code here
-            return View();
+            List<Notification> notifications = HttpContext.Session.GetObjectFromJson<List<Notification>>("Notifications");
+            var viewModel = new ListModels
+            {
+                Notifications = notifications
+            };
+            return View(viewModel);
         }
 
         public IActionResult PTRegister()
         {
             // TODO: Your code here
-            return View();
+            List<Notification> notifications = HttpContext.Session.GetObjectFromJson<List<Notification>>("Notifications");
+            var viewModel = new ListModels
+            {
+                Notifications = notifications
+            };
+            return View(viewModel);
         }
         
         

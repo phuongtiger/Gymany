8121d5997e6a504f2e30344050ee0b37f78934af
Nguyen Van Khoi <khoinvce170049@fpt.edu.vn>
2024-03-03 12:58:45 +0700

    1

--- obj/Debug/net5.0/Gymany.AssemblyInfo.cs
+++ obj/Debug/net5.0/Gymany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73fb943ff30c887f2b2b6181436e9b196186d29c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2c380ba20e4691791226e5c04cbd248c90d3d9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

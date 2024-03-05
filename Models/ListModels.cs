adf6a27fb5f3320c8e39c383896b3f4f77a3ee84
minhthang29 <minhthang32095@gmail.com>
2024-03-05 17:33:34 +0700

    v5/3

--- Models/ListModels.cs
+++ Models/ListModels.cs
@@ -12,5 +12,12 @@ namespace Gymany.Models
         public List<Product> Products { get; set; }
         public List<Notification> Notifications { get; set; }
         public string NumberNoti { get; set; }
+
+
+        //thang
+        public Cart cart{get; set;}
+        public List<Cart> Carts { get; set; }
+        public Post post{get; set;}
+        public List<Post> Posts { get; set; }
     }
 }

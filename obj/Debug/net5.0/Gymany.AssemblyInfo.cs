746229ade3a074c6d182035850571975a6c51d31
Thanhdt23 <thanhduong785@gmail.com>
2024-03-10 14:32:19 +0700

    merge

--- obj/Debug/net5.0/Gymany.AssemblyInfo.cs
+++ obj/Debug/net5.0/Gymany.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea815a196d3d346b1306bd936b251e9985b0de5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36636485dd15a5d47a030a607a8d899f8cb0bcb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gymany")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

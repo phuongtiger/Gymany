cdc95ce922a36dd6039b31e88678b95c393e7b53
Nguyen Van Khoi <khoinvce170049@fpt.edu.vn>
2024-03-16 17:05:03 +0700

    Payment: View payment history

--- Models/ListModels.cs
+++ Models/ListModels.cs
@@ -8,17 +8,17 @@ namespace Gymany.Models
 {
     public class ListModels
     {
-        public Product product{get; set;}
-        public Customer customer {get; set;}
+        public Product product { get; set; }
+        public Customer customer { get; set; }
         public List<Product> Products { get; set; }
         public IEnumerable<Product> ListProducts { get; set; }
         public List<Notification> Notifications { get; set; }
         public List<Category> Categories { get; set; }
         public string NumberNoti { get; set; }
-  
 
 
-        public Member member {get ; set ;}
+
+        public Member member { get; set; }
         public List<WorkoutPlan> workoutPlans { get; set; }
         public bool CheckMember;
         public bool CheckPayment;
@@ -26,19 +26,23 @@ namespace Gymany.Models
         {
             throw new NotImplementedException();
         }
-        public List<Member> members {get ; set ;}
+        public List<Member> members { get; set; }
 
 
 
         //thang
-        public Cart cart{get; set;}
+        public Cart cart { get; set; }
         public List<Cart> Carts { get; set; }
-        public Post post{get; set;}
+        public Post post { get; set; }
         public IEnumerable<Post> Posts { get; set; }
         public Order order { get; set; }
         public List<Order> orders { get; set; }
         public IEnumerable<Order> Orders { get; set; }
         //public List<Post> Posts { get; set; }
+
+        public Payment payment { get; set; }
+        public List<Payment> payments { get; set; }
+        public IEnumerable<Payment> Payments { get; set; }
 
     }
 }
